60a7a99b39818cf364328962eaee2d8e5c22dde0
Tariq <tariqjjj111@gmail.com>
2026-01-07 00:36:02 -0800

    challanges added

---
+++ challanges.dart
@@ -0,0 +1,49 @@
+
+/* ----------------------------------------
+    DART CHALLANGES
+ ---------------------------------------- */
+
+ /* EASY CHALLANGES
+    1 -> Write program that sum two numbers
+      TIPS: USE PLUS(+) OPERATORS
+      BONUS: TAKE RUNTIME INPUTS FROM USER
+      E.G -> INPUT NUMBERS: (2,3), OUTPUT: 5
+    2 -> Write program that compare given two numbers
+      TIPS: use relational operators (>, <, ==) to print which one is greater or if they are equal.
+      BONUS: TAKE RUNTIME INPUTS FROM USER
+      E.G -> INPUT NUMBERS: (2,3), OUTPUT: BASED ON OPERATORS
+    3 -> Write a Dart program that takes an integer and prints its square
+      TIPS: using the multiplication operator (*)
+
+ */
+
+ /* INTERMEDIATE CHALLANGES
+    1 ->  Write program that find odd and even numbers
+      TIPS: USE if, else statement
+      BONUS: TAKE RUNTIME INPUTS FROM USER
+      E.G -> INPUT NUMBERS: 22 , OUTPUT:  22 is even
+    2 -> Write a program that checks if a student’s score is between 50 and 100 
+      TIPS: using logical operators (&&, ||).
+      BONUS: USE GRADES(A+,A,B,C,D)
+    3 -> Evaluate the result of: int result = 10 + 5 * 2 - 8 / 2;
+      TIPS: Based On Operator Precedence
+      BONUS: Show step-by-step how Dart applies operator precedence.
+
+ */
+ /* HARD CHALLANGES
+    1 -> Write program that change decimal number into binary
+      TIPS: USE LOOP, MODULES(%) AND DIVISION(/) OPERATORS 
+      BONUS: TAKE RUNTIME INPUT FROM USER
+      E.G -> INPUT NUMBER: 12, OUTPUT: 1100
+
+    2 -> Write program that change binary number into decimal
+      TIPS: USE LOOP, MODULES(%) AND DIVISION(/) OPERATORS
+      BONUS: TAKE RUNTIME INPUT FROM USER
+      E.G -> INPUT NUMBER: 1100, OUTPUT: 12
+
+    3 -> Write program that sum the digits of an input number
+      TIPS: USE LOOP, MODULES(%) AND DIVISION(/) OPERATORS
+      BONUS: TAKE RUNTIME INPUT FROM USER
+      E.G -> INPUT : 12, OUTPUT (1 + 2): 3 
+
+ */
